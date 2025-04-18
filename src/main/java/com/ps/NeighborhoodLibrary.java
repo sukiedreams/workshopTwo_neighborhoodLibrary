1a14e55c15a6445ba3aa49097a394c22afe48ce7
suley ayala <suleyayala@suleyayala24.com>
2025-04-18 13:39:19 -0400

    first commit

---
+++ src/main/java/com/ps/NeighborhoodLibrary.java
@@ -0,0 +1,9 @@
+package com.ps;
+import java.util.Scanner;
+
+public class NeighborhoodLibrary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
